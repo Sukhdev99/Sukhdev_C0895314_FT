07ac422d0e2e981f82cd8e08b37bfb5db6ad8239
Sukhdev99 <sukhdev0199singh@gmail.com>
2023-12-09 11:13:55 -0500

    create left and right variable for operands in binary class

--- src/Binary.java
+++ src/Binary.java
@@ -1,4 +1,12 @@
 public abstract class Binary extends ArithmeticExp{
 
-   
+    public ArithmeticExp left;
+
+    public ArithmeticExp right;
+
+    public Binary(ArithmeticExp left, ArithmeticExp right){
+        this.left = left;
+        this.right = right;
+    }
+
 }
